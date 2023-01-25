e61c6bc32d87df2cfc8f6f4f9e2e685878bf0a6c
Thomas Powell <twilliampowell@gmail.com>
2023-01-25 08:58:53 -0500

    Multi-platform

--- directory_test.rb
+++ directory_test.rb
@@ -1,17 +1,33 @@
 # create a file named "ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ", which is ASCII 128..154
-filename=(128..154).to_a.pack('c*').force_encoding(Encoding::IBM437)
+FILENAME=(128..154).to_a.pack('c*').force_encoding(Encoding::IBM437)
+UTF_8_FILENAME=FILENAME.encode(Encoding::UTF_8)
 
 Dir.mkdir('tmp') unless Dir.exist?('tmp')
-File.open(File.join('tmp', filename.encode(Encoding::UTF_8)), 'wt') do |f|
+File.open(File.join('tmp', UTF_8_FILENAME), 'wt') do |f|
   f.puts "test"
 end
 
-output=%x|dir tmp|
 
-
-Encoding.list.each do |encoding|
-  if output.force_encoding(encoding).encode(Encoding::UTF_8).include?(filename.encode(Encoding::UTF_8))
-    puts encoding
+def check_encodings(output)
+  if output.include?(UTF_8_FILENAME)
+    puts "output matches without forcing encoding"
   end
-rescue
+
+  Encoding.list.each do |encoding|
+    if output.force_encoding(encoding).encode(Encoding::UTF_8).include?(UTF_8_FILENAME)
+      puts encoding
+    end
+  rescue
+  end
 end
+
+
+command=RUBY_PLATFORM =~ /mingw/ ? 'dir tmp' : 'ls tmp'
+puts
+puts "%x|#{command}|"
+puts "-" * (command.length + 4)
+check_encodings(%x|#{command}|)
+puts
+puts "IO.popen(#{command}).read"
+puts "-" * (command.length + 15)
+check_encodings(IO.popen(command).read)
