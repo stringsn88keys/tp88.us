e594b960d6df270680066d62b3da10f44b81ad1e
Thomas Powell <twilliampowell@gmail.com>
2023-09-19 15:34:18 +0000

    compare hostname output versions
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

---
+++ compare_hostname_output.rb
@@ -0,0 +1,30 @@
+require "socket" unless defined?(Socket)
+require "ipaddr" unless defined?(IPAddr)
+
+def resolve_fqdn
+
+    hostname = %x{hostname}.chomp
+    addrinfo = Socket.getaddrinfo(hostname, nil).first
+    iaddr = IPAddr.new(addrinfo[3])
+    Socket.gethostbyaddr(iaddr.hton)[0]
+rescue
+    nil
+end
+
+def canonicalize_hostname(hostname)
+    Addrinfo.getaddrinfo(hostname, nil, nil, nil, nil, Socket::AI_CANONNAME).first.canonname
+end
+
+def canonicalize_hostname_with_retries(hostname)
+    retries = 3
+    begin
+        canonicalize_hostname(hostname)
+    rescue
+        retries -= 1
+        retry if retries > 0
+        nil
+    end
+end
+
+puts "old code: #{resolve_fqdn}"
+puts "new code: #{canonicalize_hostname_with_retries(%x{hostname}.chomp)}"
