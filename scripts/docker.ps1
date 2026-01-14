e4250da7b111c3b323403410bc4fc76a3f66f3b3
Thomas Powell <twilliampowell@gmail.com>
2026-01-14 15:45:13 -0500

    Remove unused ps1
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- scripts/docker.ps1
+++
@@ -1,4 +0,0 @@
-cd docker
-docker build -t myopensslandruby .
-cd ..
-docker run -it -v ${PWD}:/workdir --workdir /workdir myopensslandruby sh
--- scripts/ffi.ps1
+++
@@ -1,9 +0,0 @@
-## run a focused test
-set-executionpolicy unrestricted
-git checkout tp/ffi-378-branch-install
-$ENV:CUSTOM_CHEF_POWERSHELL_BRANCH="tp/debug-ffi-yajl"
-$ENV:CHEF_LICENSE="accept-no-persist"
-bundle install
-bundle exec ruby .\post-bundle-install.rb
-bundle exec rspec .\spec\integration\client\client_spec.rb
-bundle exec rspec spec/functional/resource/windows_certificate_spec.rb
